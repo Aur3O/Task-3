a2629d1e9621c890009350a4c1052e2053684552
Aurélien RICOL <AE9517@student.jamk.fi>
2023-09-30 15:52:49 +0300

    programme completed

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -10,6 +10,42 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter distance traveled : ");
+            string input = Console.ReadLine();
+            int km;
+            double cons, price;
+            if (int.TryParse(input, out km))
+            {
+                cons = Travel.Consumption(km);
+                price = Travel.Price(km);
+                Console.WriteLine("Fuel consumption is " + cons + " liters and it costs " + price + " euros.");
+            }
+
+            else
+            {
+                Console.WriteLine("Please enter a number");
+            }
+        }
+    }
+
+    public static class Travel
+    {
+        private static double cons;
+        private static double price;
+        public static double Consumption(int km)
+        {
+            Random rnd = new Random();
+            cons = rnd.Next(6, 9);
+            cons = km * (cons / 100);
+            return cons;
+        }
+        public static double Price(int km)
+        {
+            Random rnd = new Random();
+            price = rnd.Next(175, 250);
+            price = cons * (price / 100);
+            price = Math.Round(price, 2);
+            return price;
         }
     }
 }
